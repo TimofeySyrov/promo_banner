cd86d0fa291c7cf2121385d50a224bf6a51c90ed
Timofey Syromyatnikov <warfacetopic@mail.ru>
2022-02-07 17:45:37 +0300

    Добавление вёрстки PromoBanner компонента

--- src/components/PromoBanner/PromoBanner.tsx
+++ src/components/PromoBanner/PromoBanner.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React from "react";
 
-const PromoBanner = () => {
+import Button from "../button/Button";
+import styles from "./promoBanner.module.css";
+
+type PromoBannerProps = {
+  title: JSX.Element | string;
+  imgSrc: string;
+  imgTitle: JSX.Element | string;
+  buttonName: string;
+}
+
+const PromoBanner = ({ title, imgSrc, imgTitle, buttonName }: PromoBannerProps) => {
   return (
-    <div>
-      
+    <div className={styles.promoBanner}>
+      <div className={styles.promoBanner__title}>
+        {title}
+      </div>
+      <div className={styles["promoBanner__qr-code-box"]}>
+        <img src={imgSrc} alt="qr-code" />
+        {imgTitle}
+      </div>
+      <div className={styles["promoBanner__confirm-button"]}>
+        <Button active>{buttonName}</Button>
+      </div>
     </div>
   );
 };
