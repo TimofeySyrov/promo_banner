dcee3f54736fe1a8f738848ce3776fbcaeaf5a06
Timofey Syromyatnikov <warfacetopic@mail.ru>
2022-02-07 17:44:50 +0300

    Добавление button компонента

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,31 @@
+import React, { ReactPropTypes } from "react";
+
+import styles from "./button.module.css";
+
+interface ButtonProps extends ReactPropTypes {
+  children: React.ReactNode;
+  active: boolean;
+  disabled: boolean;
+}
+
+const Button = ({
+  children,
+  active = false,
+  disabled = false,
+}: Partial<ButtonProps>) => {
+  const getClassNames = (): string => {
+    if (disabled) {
+      return `${styles.button} ${styles.button_disabled}`;
+    }
+
+    if (!disabled && active) {
+      return `${styles.button} ${active && styles.button_active}`;
+    }
+
+    return `${styles.button}`;
+  };
+
+  return <div className={getClassNames()}>{children}</div>;
+};
+
+export default Button;
