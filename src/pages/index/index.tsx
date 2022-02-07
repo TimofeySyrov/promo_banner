0987b727defb987a6e6783d730c692cf3f3dc03d
Timofey Syromyatnikov <warfacetopic@mail.ru>
2022-02-07 18:13:08 +0300

    Добавление видео с баннером на index страницу

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React from "react";
+
+import qrCodeImg from "../../static/images/promo-qr-code.svg";
+import volvoTrucksVideo from "../../static/video/VolvoTrucks.mp4";
+import PromoBanner from "../../components/PromoBanner/PromoBanner";
+import styles from "./index.module.css";
 
 const Index = () => {
   return (
-    <div>
-      hello
+    <div className={styles.index}>
+      <div className={styles.promo}>
+        <div className={styles.promo__video}>
+          <video autoPlay muted>
+            <source src={volvoTrucksVideo} type="video/mp4"></source>
+          </video>
+        </div>
+        <div className={styles.promo__banner}>
+          <PromoBanner
+            title={
+              <>
+                Исполните мечту вашего малыша!
+                <br />
+                Подарите ему собаку!
+              </>
+            }
+            imgSrc={qrCodeImg}
+            imgTitle="Сканируйте QR-код или нажмите ОК"
+            buttonName="Ок"
+          />
+        </div>
+      </div>
     </div>
   );
 };
